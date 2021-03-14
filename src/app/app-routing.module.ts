5c3f39c13805b2057e4333ee3bf9a0bba0a3a810
Allan Guerra <allan.guerra19@gmail.com>
2021-03-14 20:03:01 -0300

    feat: add signup route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'signup'
+  },
+  {
+    path: 'signup',
+    loadChildren: () => import('./modules/signup/signup.module').then(module => module.SignupModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
