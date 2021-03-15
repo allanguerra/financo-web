64ddbbd4caa071ff0084d0cfe49c1022213a1125
Allan Guerra <allan.guerra19@gmail.com>
2021-03-15 13:20:13 -0300

    feat: create signup component

--- src/app/modules/join/join.module.ts
+++ src/app/modules/join/join.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { SharedModule } from '@src/app/shared/shared.module';
 import { JoinRoutingModule } from '@src/app/modules/join/join-routing.module';
 
+import { SignupComponent } from '@src/app/modules/join/components/signup/signup.component';
+
 
 @NgModule({
-  declarations: [],
+  declarations: [SignupComponent],
   imports: [
     SharedModule,
     JoinRoutingModule
