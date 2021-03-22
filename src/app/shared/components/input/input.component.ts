34bb0509f8be4cb0cd997842b859b5c35b455cc5
Allan Guerra <allan.guerra19@gmail.com>
2021-03-22 01:26:56 -0300

    feat: add tooltip to input component

--- src/app/shared/components/input/input.component.ts
+++ src/app/shared/components/input/input.component.ts
@@ -11,6 +11,9 @@ export class InputComponent implements OnInit {
   @Input('label')  public label: string;
   @Input('control') control: FormControl;
   @Input('dark') public dark: boolean = false;
+  @Input('tooltip') public tooltip: string;
+
+  public show: boolean = false;
 
   constructor() { }
 
@@ -23,9 +26,20 @@ export class InputComponent implements OnInit {
   public getMessage(): string | null {
     if (this.control.errors && this.control.errors.required) {
       return 'campo obrigatório';
+    } else if (this.control.errors && this.control.errors.email) {
+      return 'não é um email válido';
     } else if (this.control.errors && this.control.errors.minlength) {
       return `mínimo de ${this.control.errors.minlength.requiredLength} caracteres`;
     }
     return null;
+  }
+
+  public showTooltip(): void {
+    if (this.show) {
+      this.show = false;
+    } else {
+      this.show = true;
+      setTimeout(() => { this.show = false; }, 4000);
+    }
   }
 }
