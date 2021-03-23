1cd7b05ae2dde8f5e395f27710ca0ef384fde0b6
Allan Guerra <allan.guerra19@gmail.com>
2021-03-23 19:26:49 -0300

    refactor: fix angular issue to abstract components

--- src/app/shared/base-components/base-join/base-join.component.ts
+++ src/app/shared/base-components/base-join/base-join.component.ts
@@ -1,9 +1,10 @@
-import { Injector, OnInit } from '@angular/core';
+import { Component, Injectable, Injector, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { Router } from '@angular/router';
 
 import { SIZES } from '@src/app/utils/consts';
 
+@Component({ template: '' })
 export abstract class BaseJoinComponent implements OnInit {
 
   public modelForm: FormGroup;
