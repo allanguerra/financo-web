da7e6d2205d9dbfa57485ccae8318a8336863a60
Allan Guerra <allan.guerra19@gmail.com>
2021-04-11 13:38:01 -0300

    test: add get forgot status and change forgot password methods to forgot service

--- src/app/modules/join/services/forgot-service/forgot.service.spec.ts
+++ src/app/modules/join/services/forgot-service/forgot.service.spec.ts
@@ -1,5 +1,6 @@
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
+import { ChangePassword } from '@src/app/modules/join/models/change-password.model';
 import { Forgot } from '@src/app/modules/join/models/forgot.model';
 
 import { ForgotService } from './forgot.service';
@@ -33,6 +34,38 @@ describe('ForgotService', () => {
     });
 
     httpMock.expectOne('http://localhost:3000/v1/users/forgot')
+      .flush(null, { status: 201, statusText: 'created' });
+  });
+
+  it('should do a get request to verify forgot password status endpoint', (done) => {
+    const forgotId = 'any_forgot_id';
+
+    service.verifyForgotStatus(forgotId).subscribe({
+      next: (response) => {
+        expect(response).toBeTruthy();
+        expect(response).toEqual('active');
+        done();
+      },
+      error: () => done.fail()
+    });
+
+    httpMock.expectOne('http://localhost:3000/v1/users/forgot/any_forgot_id/status')
+      .flush('active', { status: 200, statusText: 'ok' });
+  });
+
+  it('should do a patch request to change forgot password endpoint', (done) => {
+    const forgotId = 'any_forgot_id';
+    const changePassword: ChangePassword = { newPassword: 'any_password', repeatPassword: 'any_password' };
+
+    service.changeForgotPassword(forgotId, changePassword).subscribe({
+      next: (response) => {
+        expect(response).toBeFalsy();
+        done();
+      },
+      error: () => done.fail()
+    });
+
+    httpMock.expectOne('http://localhost:3000/v1/users/change-password/forgot/any_forgot_id')
       .flush(null, { status: 201, statusText: 'created' });
   });
 });
