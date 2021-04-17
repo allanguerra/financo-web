314ed42513c701fdb307b512acf686ea6a8e76b6
Allan Guerra <allan.guerra19@gmail.com>
2021-04-17 15:47:00 -0300

    feat: add click event to fab button

--- src/app/shared/components/main/main.component.ts
+++ src/app/shared/components/main/main.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FabModalComponent } from '@src/app/shared/components/fab-modal/fab-modal.component';
+import { ModalService } from '@src/app/shared/services/modal-service/modal.service';
 
 @Component({
   selector: 'app-main',
@@ -7,9 +9,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private readonly modalService: ModalService
+  ) { }
 
   ngOnInit(): void {
+  }
+
+  public openModal(): void {
+    const modal: FabModalComponent = this.modalService.openModal(FabModalComponent);
   }
 
 }
