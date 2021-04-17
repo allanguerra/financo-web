d1152929de3a846f2cb2c24566440e378514ef2a
Allan Guerra <allan.guerra19@gmail.com>
2021-04-17 14:58:37 -0300

    test: create modal service

---
+++ src/app/shared/services/modal-service/modal.service.spec.ts
@@ -0,0 +1,15 @@
+import { TestBed } from '@angular/core/testing';
+import { ModalService } from '@src/app/shared/services/modal-service/modal.service';
+
+describe('ModalService', () => {
+  let service: ModalService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    service = TestBed.inject(ModalService);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+});
