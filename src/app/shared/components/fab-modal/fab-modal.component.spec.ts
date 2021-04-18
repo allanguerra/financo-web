29a23196b3cea4011af3944d233522d06ecba67a
Allan Guerra <allan.guerra19@gmail.com>
2021-04-17 21:33:21 -0300

    test: add navigation method to categories button

--- src/app/shared/components/fab-modal/fab-modal.component.spec.ts
+++ src/app/shared/components/fab-modal/fab-modal.component.spec.ts
@@ -1,4 +1,6 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { Router } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
 import { ModalComponent } from '@src/app/shared/components/modal/modal.component';
 import { ModalService } from '@src/app/shared/services/modal-service/modal.service';
 
@@ -10,6 +12,7 @@ describe('FabModalComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [ RouterTestingModule ],
       declarations: [ FabModalComponent, ModalComponent ],
       providers: [ ModalService ]
     })
@@ -24,5 +27,22 @@ describe('FabModalComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should redirect to categories and close modal', (done) => {
+    const navigateSpy = jest.spyOn(Router.prototype, 'navigate').mockReturnValueOnce(null);
+    const modalServiceSpy = jest.spyOn(ModalService.prototype, 'closeModal').mockReturnValueOnce(null);
+
+    const categoriesButton = fixture.debugElement.nativeElement.querySelector('#categoriesButton');
+    categoriesButton.click();
+    fixture.detectChanges();
+
+    setTimeout(() => {
+      expect(navigateSpy).toHaveBeenCalledTimes(1);
+      expect(navigateSpy).toHaveBeenCalledWith(['home', 'categories']);
+      expect(modalServiceSpy).toHaveBeenCalledTimes(1);
+
+      done();
+    }, 200);
   });
 });
