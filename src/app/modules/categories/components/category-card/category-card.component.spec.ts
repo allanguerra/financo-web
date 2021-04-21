47093582544551ea8ee18c6ceed5a046e6ea7ea1
Allan Guerra <allan.guerra19@gmail.com>
2021-04-21 14:59:35 -0300

    test: create category card component

---
+++ src/app/modules/categories/components/category-card/category-card.component.spec.ts
@@ -0,0 +1,33 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { CategoryCardComponent } from './category-card.component';
+
+describe('CategoryCardComponent', () => {
+  let component: CategoryCardComponent;
+  let fixture: ComponentFixture<CategoryCardComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ CategoryCardComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(CategoryCardComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should toggle menu when click on menu icon', () => {
+    const menuButton = fixture.debugElement.nativeElement.querySelector('.fas.fa-ellipsis-v');
+    menuButton.click();
+    fixture.detectChanges();
+
+    expect(component.showMenu).toBe(true);
+  });
+});
