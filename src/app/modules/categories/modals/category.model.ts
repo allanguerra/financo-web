331153526798e1d510cf0ed650d5e364125d5cbc
Allan Guerra <allan.guerra19@gmail.com>
2021-04-21 14:52:45 -0300

    refactor: add method to assign objects received from backend

--- src/app/modules/categories/modals/category.model.ts
+++ src/app/modules/categories/modals/category.model.ts
@@ -2,8 +2,31 @@ import { BaseModel } from '@src/app/shared/models/base.model';
 import { CategoryType } from '@src/app/shared/enums/category-type.enum';
 
 export class Category extends BaseModel {
-  name: string;
-  description: string;
-  color: string;
-  type: CategoryType;
+  constructor(
+    public name?: string,
+    public description?: string,
+    public color?: string,
+    public type?: CategoryType,
+  ) {
+    super();
+  }
+
+  // STATIC METHODS
+
+  static fromData(data: any): Category {
+    return Object.assign(new Category(), data);
+  }
+
+  // PUBLIC METHODS
+
+  public getType(): string {
+    switch (this.type) {
+      case CategoryType.BOTH:
+        return 'AMBOS';
+      case CategoryType.EXPENSE:
+        return 'DESPESAS';
+      case CategoryType.REVENUE:
+        return 'RECEITAS';
+    }
+  }
 }
--- src/app/shared/base-services/base-model-service/base-model.service.ts
+++ src/app/shared/base-services/base-model-service/base-model.service.ts
@@ -4,6 +4,7 @@ import { BoardsService } from '@src/app/modules/boards/services/boards-service/b
 import { BaseModel } from '@src/app/shared/models/base.model';
 import { Messages } from '@src/app/utils/messages';
 import { Observable, throwError } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 export abstract class BaseModelService<T extends BaseModel> {
 
@@ -13,6 +14,7 @@ export abstract class BaseModelService<T extends BaseModel> {
   constructor(
     protected readonly apiPath: string,
     protected readonly injector: Injector,
+    protected readonly dataToModelFn: (data: any) => T
   ) {
     this.http = injector.get(HttpClient);
     this.boardsService = injector.get(BoardsService);
@@ -23,6 +25,22 @@ export abstract class BaseModelService<T extends BaseModel> {
     if (!boardId) {
       return throwError(Messages.ACTIVE_BOARD_NOT_FOUND);
     }
-    return this.http.get<Array<T>>(`${this.apiPath.replace(':boardId', boardId)}`);
+    return this.http.get<Array<T>>(`${this.apiPath.replace(':boardId', boardId)}`)
+      .pipe(
+        map(this.dataToModelArray.bind(this))
+      );
+  }
+
+  // PROTECTED METHODS
+
+  protected dataToModel(data: any): T {
+    return this.dataToModelFn(data);
+  }
+
+  protected dataToModelArray(data: Array<any>): Array<T> {
+    const modelArray: Array<T> = [];
+    data.forEach((dataItem: any) => modelArray.push(this.dataToModel(dataItem)));
+
+    return modelArray;
   }
 }
