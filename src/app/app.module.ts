2a4d429bb2448644d07f0b7c7c4d1ae03cf4666a
Allan Guerra <allan.guerra19@gmail.com>
2021-05-12 23:19:43 -0300

    chore: add localization and animations module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { ErrorHandler, NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ErrorHandler, LOCALE_ID, NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from '@src/app/app-routing.module';
@@ -9,6 +10,10 @@ import { SharedModule } from '@src/app/shared/shared.module';
 import { INTERCEPTORS_PROVIDER } from '@src/app/infra/interceptors';
 import { ApplicationErrorHandler } from '@src/app/infra/error/error.handler';
 
+import { registerLocaleData } from '@angular/common';
+import ptBr from '@angular/common/locales/pt';
+registerLocaleData(ptBr);
+
 
 @NgModule({
   declarations: [
@@ -16,6 +21,7 @@ import { ApplicationErrorHandler } from '@src/app/infra/error/error.handler';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     HttpClientModule,
     AppRoutingModule,
     SharedModule
@@ -25,6 +31,10 @@ import { ApplicationErrorHandler } from '@src/app/infra/error/error.handler';
     {
       provide: ErrorHandler,
       useClass: ApplicationErrorHandler
+    },
+    {
+      provide: LOCALE_ID,
+      useValue: 'pt'
     }
   ],
   bootstrap: [AppComponent]
