d87864baa504aaea4af8fb86e1f38cad9ad4931d
Allan Guerra <allan.guerra19@gmail.com>
2021-05-14 01:25:18 -0300

    test: fix unit test of expenses form service

--- src/app/modules/expenses/services/expenses-form-service/expenses-form.service.spec.ts
+++ src/app/modules/expenses/services/expenses-form-service/expenses-form.service.spec.ts
@@ -1,3 +1,4 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
 
 import { ExpensesFormService } from './expenses-form.service';
@@ -6,7 +7,9 @@ describe('ExpensesFormService', () => {
   let service: ExpensesFormService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule]
+    });
     service = TestBed.inject(ExpensesFormService);
   });
 
