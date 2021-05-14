6b8cfb1ce2b18b2ce639e707051f7424cde77db1
Allan Guerra <allan.guerra19@gmail.com>
2021-05-14 01:12:47 -0300

    test: add redirect to expense and redirect to revenue

--- src/app/shared/components/fab-modal/fab-modal.component.spec.ts
+++ src/app/shared/components/fab-modal/fab-modal.component.spec.ts
@@ -1,10 +1,10 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { Router } from '@angular/router';
 import { RouterTestingModule } from '@angular/router/testing';
+import { FabModalComponent } from '@src/app/shared/components/fab-modal/fab-modal.component';
 import { ModalComponent } from '@src/app/shared/components/modal/modal.component';
 import { ModalService } from '@src/app/shared/services/modal-service/modal.service';
 
-import { FabModalComponent } from './fab-modal.component';
 
 describe('FabModalComponent', () => {
   let component: FabModalComponent;
@@ -25,6 +25,10 @@ describe('FabModalComponent', () => {
     fixture.detectChanges();
   });
 
+  beforeEach(() => {
+    jest.restoreAllMocks();
+  });
+
   it('should create', () => {
     expect(component).toBeTruthy();
   });
@@ -40,6 +44,40 @@ describe('FabModalComponent', () => {
     setTimeout(() => {
       expect(navigateSpy).toHaveBeenCalledTimes(1);
       expect(navigateSpy).toHaveBeenCalledWith(['home', 'categories']);
+      expect(modalServiceSpy).toHaveBeenCalledTimes(1);
+
+      done();
+    }, 200);
+  });
+
+  it('should redirect to expenses and close modal', (done) => {
+    const navigateSpy = jest.spyOn(Router.prototype, 'navigate').mockReturnValueOnce(null);
+    const modalServiceSpy = jest.spyOn(ModalService.prototype, 'closeModal').mockReturnValueOnce(null);
+
+    const categoriesButton = fixture.debugElement.nativeElement.querySelector('#expenseButton');
+    categoriesButton.click();
+    fixture.detectChanges();
+
+    setTimeout(() => {
+      expect(navigateSpy).toHaveBeenCalledTimes(1);
+      expect(navigateSpy).toHaveBeenCalledWith(['home', 'expenses']);
+      expect(modalServiceSpy).toHaveBeenCalledTimes(1);
+
+      done();
+    }, 200);
+  });
+
+  it('should redirect to revenues and close modal', (done) => {
+    const navigateSpy = jest.spyOn(Router.prototype, 'navigate').mockReturnValueOnce(null);
+    const modalServiceSpy = jest.spyOn(ModalService.prototype, 'closeModal').mockReturnValueOnce(null);
+
+    const categoriesButton = fixture.debugElement.nativeElement.querySelector('#revenueButton');
+    categoriesButton.click();
+    fixture.detectChanges();
+
+    setTimeout(() => {
+      expect(navigateSpy).toHaveBeenCalledTimes(1);
+      expect(navigateSpy).toHaveBeenCalledWith(['home', 'revenues']);
       expect(modalServiceSpy).toHaveBeenCalledTimes(1);
 
       done();
