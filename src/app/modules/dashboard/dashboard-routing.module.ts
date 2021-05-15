68390efcc68651612011c58c6d6bafa671f69633
Allan Guerra <allan.guerra19@gmail.com>
2021-05-15 19:44:45 -0300

    feat: create dashboard component

--- src/app/modules/dashboard/dashboard-routing.module.ts
+++ src/app/modules/dashboard/dashboard-routing.module.ts
@@ -6,6 +6,7 @@ import { AuthGuard } from '@src/app/infra/guards/auth-guard/auth.guard';
 import { HomeComponent } from '@src/app/modules/dashboard/components/home/home.component';
 import { ExpensesListComponent } from '@src/app/modules/dashboard/components/expenses-list/expenses-list.component';
 import { RevenuesListComponent } from '@src/app/modules/dashboard/components/revenues-list/revenues-list.component';
+import { DashboardComponent } from '@src/app/modules/dashboard/components/dashboard/dashboard.component';
 
 const routes: Routes = [
   {
@@ -13,6 +14,15 @@ const routes: Routes = [
     component: HomeComponent,
     canActivate: [ AuthGuard ],
     children: [
+      {
+        path: '',
+        redirectTo: 'balance',
+        pathMatch: 'full'
+      },
+      {
+        path: 'balance',
+        component: DashboardComponent,
+      },
       {
         path: 'balance/revenues',
         component: RevenuesListComponent
--- src/app/modules/dashboard/dashboard.module.ts
+++ src/app/modules/dashboard/dashboard.module.ts
@@ -6,13 +6,15 @@ import { SharedModule } from '@src/app/shared/shared.module';
 import { HomeComponent } from '@src/app/modules/dashboard/components/home/home.component';
 import { RevenuesListComponent } from '@src/app/modules/dashboard/components/revenues-list/revenues-list.component';
 import { ExpensesListComponent } from '@src/app/modules/dashboard/components/expenses-list/expenses-list.component';
+import { DashboardComponent } from '@src/app/modules/dashboard/components/dashboard/dashboard.component';
 
 
 @NgModule({
   declarations: [
     HomeComponent,
     RevenuesListComponent,
-    ExpensesListComponent
+    ExpensesListComponent,
+    DashboardComponent
   ],
   imports: [
     SharedModule,
