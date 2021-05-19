4e1934103f7f808938deeda6cc6a9ceace459a10
Allan Guerra <allan.guerra19@gmail.com>
2021-05-19 18:45:06 -0300

    test: create boards form service

--- src/app/modules/boards/services/boards-form-service/boards-form.service.spec.ts
+++ src/app/modules/boards/services/boards-form-service/boards-form.service.spec.ts
@@ -1,3 +1,4 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
 
 import { BoardsFormService } from './boards-form.service';
@@ -6,7 +7,9 @@ describe('BoardsFormService', () => {
   let service: BoardsFormService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [ HttpClientTestingModule ]
+    });
     service = TestBed.inject(BoardsFormService);
   });
 
