f182217e88250ebacee3904d315359c99a990d48
Allan Guerra <allan.guerra19@gmail.com>
2021-05-19 16:37:01 -0300

    test: create boards list service

--- src/app/modules/boards/services/boards-list-service/boards-list.service.spec.ts
+++ src/app/modules/boards/services/boards-list-service/boards-list.service.spec.ts
@@ -1,3 +1,4 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
 
 import { BoardsListService } from './boards-list.service';
@@ -6,7 +7,11 @@ describe('BoardsListService', () => {
   let service: BoardsListService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [
+        HttpClientTestingModule
+      ]
+    });
     service = TestBed.inject(BoardsListService);
   });
 
