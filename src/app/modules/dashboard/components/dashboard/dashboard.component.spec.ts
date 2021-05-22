ebff912b3e0d30fe731654f346e57077e0452155
Allan Guerra <allan.guerra19@gmail.com>
2021-05-22 20:49:17 -0300

    test: create dashboard component

--- src/app/modules/dashboard/components/dashboard/dashboard.component.spec.ts
+++ src/app/modules/dashboard/components/dashboard/dashboard.component.spec.ts
@@ -1,4 +1,12 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ResumeCardComponent } from '@src/app/modules/dashboard/components/resume-card/resume-card.component';
+import { Dashboard } from '@src/app/modules/dashboard/models/dashboard-model';
+import { DashboardService } from '@src/app/modules/dashboard/services/dashboard-service/dashboard.service';
+import { ChartComponent } from '@src/app/shared/components/chart/chart.component';
+import { LoadderComponent } from '@src/app/shared/components/loadder/loadder.component';
+import { ChartModule } from 'primeng/chart';
+import { of } from 'rxjs';
 
 import { DashboardComponent } from './dashboard.component';
 
@@ -8,7 +16,16 @@ describe('DashboardComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ DashboardComponent ]
+      imports: [
+        HttpClientTestingModule,
+        ChartModule,
+      ],
+      declarations: [
+        DashboardComponent,
+        ResumeCardComponent,
+        ChartComponent,
+        LoadderComponent
+      ]
     })
     .compileComponents();
   }));
@@ -17,6 +34,10 @@ describe('DashboardComponent', () => {
     fixture = TestBed.createComponent(DashboardComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+  });
+
+  afterAll(() => {
+    jest.restoreAllMocks();
   });
 
   it('should create', () => {
