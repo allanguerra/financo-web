3d0d9f09fe63d28e2f5acb52cb93c7fc531b177c
Allan Guerra <allan.guerra19@gmail.com>
2021-05-22 20:48:43 -0300

    feat: create dashboard component

--- src/app/modules/dashboard/components/dashboard/dashboard.component.ts
+++ src/app/modules/dashboard/components/dashboard/dashboard.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Dashboard } from '@src/app/modules/dashboard/models/dashboard-model';
+import { DashboardService } from '@src/app/modules/dashboard/services/dashboard-service/dashboard.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -7,9 +9,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
-  constructor() { }
+  public isLoading: boolean = false;
+
+  public dashboard: Dashboard;
+
+  private readonly currentDate: Date = new Date();
+
+  constructor(
+    private readonly dashboardService: DashboardService
+  ) { }
 
   ngOnInit(): void {
+    this.getDashboard();
+  }
+
+  // PRIVATE METHODS
+
+  private getDashboard(): void {
+    this.isLoading = true;
+    const params = { month: this.currentDate.getMonth() + 1, year: this.currentDate.getFullYear() };
+
+    this.dashboardService.getDashboard(params).subscribe({
+      next: (dashboard: Dashboard) => {
+        this.dashboard = dashboard;
+        this.isLoading = false;
+      }
+    });
   }
 
 }
