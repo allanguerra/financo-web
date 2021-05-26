8b6964600694ec5bd550352d6f2e5fefee83c87f
Allan Guerra <allan.guerra19@gmail.com>
2021-05-26 20:11:31 -0300

    test: add show board and share board methods to boards service

--- src/app/modules/boards/services/boards-service/boards.service.spec.ts
+++ src/app/modules/boards/services/boards-service/boards.service.spec.ts
@@ -1,6 +1,7 @@
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
 import { Board } from '@src/app/modules/boards/models/board.model';
+import { SESSION } from '@src/app/utils/consts';
 
 import { BoardsService } from './boards.service';
 
@@ -65,6 +66,41 @@ describe('BoardsService', () => {
         owner: 'any_owner',
         sharedUsers: []
       }], { status: 200, statusText: 'ok' });
+  });
+
+  it('should return a user s board', (done) => {
+    service.getBoard('any_id').subscribe({
+      next: (board: Board) => {
+        expect(board).toBeTruthy();
+        expect(board._id).toEqual('any_id');
+        expect(board.title).toEqual('any_title');
+        expect(board.description).toEqual('any_description');
+        expect(board.owner).toEqual('any_owner');
+        done();
+      },
+      error: () => done.fail()
+    });
+
+    httpMock.expectOne('http://localhost:3000/v1/boards/any_id')
+      .flush({
+        _id: 'any_id',
+        title: 'any_title',
+        description: 'any_description',
+        owner: 'any_owner',
+        sharedUsers: []
+      }, { status: 200, statusText: 'ok' });
+  });
+
+  it('should share a user s board', (done) => {
+    sessionStorage.setItem(SESSION.ACTIVE_BOARD, 'any_id');
+
+    service.shareBoard('email@email.com').subscribe({
+      next: () => done(),
+      error: () => done.fail()
+    });
+
+    httpMock.expectOne('http://localhost:3000/v1/boards/any_id/share-to/email@email.com')
+      .flush(null, { status: 200, statusText: 'ok' });
   });
 
   it('should notify observers when change the active board', (done) => {
